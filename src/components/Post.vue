1d4da568b5f5b11ed5f96a3a0f32e6bba866786f
jahid-dev <jahidhasancmt7@gmail.com>
2023-09-18 23:22:43 +0600

    single blog page

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,13 +1,44 @@
 <script setup>
+import {useRoute} from 'vue-router'
+import {ref, reactive, onBeforeMount} from 'vue'
+import axios from 'axios'
+const post = reactive({})
+const comments = ref("")
+const route = useRoute()
+const id = route.params.id
+onBeforeMount(()=>{
+    let single_url = "https://dummyjson.com/posts/"+id
+    axios.get(single_url).then(res=>{
+        post.id = res.data.id,
+        post.title = res.data.title,
+        post.body = res.data.body
+    })
 
+    let single_comment_url = "https://dummyjson.com/posts/"+id+"/comments?limit=5"
+    axios.get(single_comment_url).then(res=>{
+        comments.value= res.data.comments
+    })
+})
 </script>
 
 <template>
-    <article class="mb-10" v-for="post in posts" :key="post.id">
-        <h1 class="text-xl mb-2"></h1>
+    <article class="mb-10">
+        <h1 class="text-xl mb-2">{{ post.title }}</h1>
         <p>
+            <img :src="`//source.unsplash.com/random/300x200?${post.id}`" alt="">
+            {{ post.body }}
         </p>
     </article>
+
+    <h3 v-if="comments.length>0">Comments</h3>
+    <hr>
+    <br>
+
+    <ul v-if="comments.length>0">
+        <li v-for="comment in comments" :key="comment.id">
+            <strong>{{ comment.user.username }}</strong> says {{comment.body}}
+        </li>
+    </ul>
 </template>
 
 <style scoped>
